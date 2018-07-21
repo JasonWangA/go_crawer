5077be448eccdc61d436b4a2b3c283a079cc789a
zhanglongdream <zhanglong_hn@163.com>
2018-07-21 16:17:59 +0800

    utf8

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"io/ioutil"
 	"fmt"
 	"golang.org/x/text/transform"
-	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/net/html/charset"
+	"io"
+	"golang.org/x/text/encoding"
+	"bufio"
 )
 
 func main()  {
@@ -18,8 +21,9 @@ func main()  {
 
 	if resp.StatusCode == http.StatusOK {
 
+		e := determingRncoding(resp.Body)
 		utfEncoding := transform.NewReader(resp.Body,
-			simplifiedchinese.GBK.NewDecoder())
+			e.NewDecoder())
 		all, err := ioutil.ReadAll(utfEncoding)
 
 		if err != nil {
@@ -28,4 +32,14 @@ func main()  {
 		fmt.Printf("%s\n" ,all)
 	}
 
+}
+
+func determingRncoding (r io.ReadCloser) encoding.Encoding{
+   bytes, err:= bufio.NewReader(r).Peek(1024)
+   if err != nil {
+   	panic(err)
+   }
+   e, _, _ := charset.DetermineEncoding(bytes, "")
+
+   return  e
 }
